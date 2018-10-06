960199273320f0a2680ee5dd06aa512ca32051b2
chudilo <chudik97@yandex.ru>
2018-10-06 05:40:43 +0300

    tried to add memory check and commented out everything but one algorythm

--- AA/lab1/main_test.cpp
+++ AA/lab1/main_test.cpp
@@ -2,7 +2,8 @@
 #include <ctime>
 #include "distfunc.h"
 //#include <unistd.h>
-
+#include <sys/time.h>
+#include <sys/resource.h>
 
 int main() {
 
@@ -23,39 +24,51 @@ int main() {
   //puts(word1.ptr);
   //puts(word2.ptr);
 
-  puts("test1");
-  double t1, t2;
+  double t1, t2, res = 0;
   int resDist;
+
 
   for(int i = 100; i <= 1000; i+= 100) { //Задать вопрос по читаемости
     word1.len = word2.len = i;
+    for(int j = 0; j < 100; j++) {
+      t1 = getCPUTime( );
+      resDist = levDist(word1, word2);
+      t2 = getCPUTime( );
 
-    t1 = getCPUTime( );
-    resDist = levDist(word1, word2);
-    t2 = getCPUTime( );
 
+      res==0?res = t2-t1:res = res/2 + (t2-t1)/2;
+    }
     printf("Levenshtein's distance for %d elements is %d \n",i, resDist);
-    printf("Proc time is: %f ms.\n\n", (t2-t1)*1000);
+    printf("Proc time is: %f ms.\n\n", res*1000);
   }
-  /*
-  for(int i = 100; i <= 1000; i+= 100) { //Задать вопрос по читаемости
+
+  struct rusage rusage;
+   if ( getrusage( RUSAGE_SELF, &rusage ) != -1 )
+       printf("memory %ld and %ld\n", rusage.ru_ixrss, rusage.ru_idrss); //this fields doesnt work on linux and have 0 as default
+ /*
+  for(int i = 0; i <= 20; i++) { //Задать вопрос по читаемости
     word1.len = word2.len = i;
     t1 = getCPUTime( );
     resDist = levDistRec(word1, word2);
     t2 = getCPUTime( );
 
-    printf("Levenshtein's reccurent distance for %d elements is %d \n"i, resDist);
-    printf("Proc time is: %f ms.\n\n", (t2-t1)*1000);
-  */
+    res = t2-t1;
+    printf("Levenshtein's reccurent distance for %d elements is %d \n", i, resDist);
+    printf("Proc time is: %f ms.\n\n", res*1000);
+  }
 
+  res = 0;
   for(int i = 100; i <= 1000; i+= 100) { //Задать вопрос по читаемости
     word1.len = word2.len = i;
+    for(int j =0; j < 100; j++) {
+      t1 = getCPUTime( );
+      resDist = damDist(word1, word2);
+      t2 = getCPUTime( );
 
-    t1 = getCPUTime( );
-    resDist = damDist(word1, word2);
-    t2 = getCPUTime( );
-
-    printf("Damerau's distance for %d elements is %d \n",i, resDist);
-    printf("Proc time is: %f ms.\n\n", (t2-t1)*1000);
+      res==0?res = t2-t1:res = res/2 + (t2-t1)/2;
     }
+    printf("Damerau's distance for %d elements is %d \n",i, resDist);
+    printf("Proc time is: %f ms.\n\n", res*1000);
+    }
+*/
 }
