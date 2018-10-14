367c167d13bd754d1298d41328d0ed1a2dea82c5
chudilo <chudik97@yandex.ru>
2018-10-15 01:00:13 +0300

    did all the prog exept 'fors' tests

--- AA/lab2/main.cpp
+++ AA/lab2/main.cpp
@@ -3,11 +3,12 @@
 #include "matrix_func.h"
 #include "get_time.h"
 
-#define SIZE 3
+#define SIZE 1000
 
 int main() {
 
 	for(int i = 0; i < 20; i++) {
+
 		int **matrA = matrixCreate(SIZE,SIZE);
 		matrixFillRand(matrA, SIZE, SIZE);
 		//matrixPrint(matrA, SIZE, SIZE);
@@ -22,32 +23,33 @@ int main() {
 
 		matrixFillZero(resMatr, SIZE, SIZE);
 		restime = 0;
-		//t1 = 
+		t1 =  getCPUTime( );
 		matrixMult(matrA,matrB,resMatr,SIZE);
-		//t2 =
-		//restime = (t2-t1)/1000;
+		t2 =  getCPUTime( );
+		//i == 0 ? restime = (t2-t1) : restime = (restime + t2-t1)/2;
 		//matrixPrint(resMatr,SIZE,SIZE);
+		printf("%f\n", restime);
 
 		matrixFillZero(resMatr, SIZE, SIZE);
 		restime = 0;
-		//t1 =
+		t1 =  getCPUTime( );
 		matrixMultVinograd(matrA,matrB,resMatr,SIZE);
-		//t2 =
-		//restime = (t2-t1)/1000;		
+		t2 =  getCPUTime( );
+		restime = (t2-t1);
+		printf("%f\n", restime);
 		//matrixPrint(resMatr,SIZE,SIZE);
 
 		matrixFillZero(resMatr, SIZE, SIZE);
 		restime = 0;
-		//t1 =
+		t1 =  getCPUTime( );
 		matrixMultVinogradUpgr(matrA,matrB,resMatr,SIZE);
-		//t2 =
-		//restime = (t2-t1)/1000;
+		t2 =  getCPUTime( );
+		restime = (t2-t1);
+		printf("%f\n", restime);
 		//matrixPrint(resMatr,SIZE,SIZE);
 
 		matrixFree(matrA,SIZE);
 		matrixFree(matrB,SIZE);
 		matrixFree(resMatr,SIZE);
 	}
-
-
 }
--- AA/lab2/matrix_func.cpp
+++ AA/lab2/matrix_func.cpp
@@ -7,8 +7,7 @@
 using namespace std;
 
 int **matrixCreate(int rows, int columns) {
-	int **matrixPtr = (int**)malloc(rows*sizeof(int));
-
+	int **matrixPtr = (int**)malloc(rows*sizeof(int*));
 	for(int i = 0; i < rows; i++)
 		matrixPtr[i] = (int*)malloc(columns*sizeof(int));
 
@@ -17,9 +16,9 @@ int **matrixCreate(int rows, int columns) {
 
 
 void matrixFree(int ** matrixPtr, int rows) {
-	for(int i = 0; i < rows; i++)
+	for(int i = 0; i < rows; i++) {
 		free(matrixPtr[i]);
-
+	}
 	free(matrixPtr);
 }
 
@@ -29,14 +28,15 @@ int init_seed() {
 }
 
 void matrixFillRand(int** matrixPtr, int rows, int columns) {
- 	static int seed = init_seed(); 
-    for(int i = 0; i < rows; i++) 
+ 	static int seed = init_seed();
+		for(int i = 0; i < rows; i++) {
     	for(int j = 0; j < columns; j++)
-    		matrixPtr[i][j] =  std::rand()%3;
+    		matrixPtr[i][j] =  std::rand()%10;
+		}
 }
 
 void matrixFillZero(int** matrixPtr, int rows, int columns) {
- 	for(int i = 0; i < rows; i++) 
+ 	for(int i = 0; i < rows; i++)
     	for(int j = 0; j < columns; j++)
     		matrixPtr[i][j] =  0;
 
@@ -61,18 +61,18 @@ void matrixMult(int **first, int **second, int **result, int size) {
 
 
 void matrixMultVinograd(int **first, int **second, int **result, int size) {
-  
-	int *multH = (int*)malloc(size*sizeof(int));	
+
+	int *multH = (int*)malloc(size*sizeof(int));
 	int* multV = (int*)malloc(size*sizeof(int));
-	
+
 	for(int i = 0; i < size; i++)
 	{
 		multH[i] = 0;
 		multV[i] = 0;
 	}
-	
+
 	for(int i = 0; i < size; i++){ // 2 + size(2 + ...)
-		for(int j = 0; j < size/2; j++){ //3 + size/2*(3 + ...) 
+		for(int j = 0; j < size/2; j++){ //3 + size/2*(3 + ...)
 			multH[i] = multH[i] + first[i][2*j]*first[i][2*j+1]; // 1+1+1+1+3+1+4 = 12
 		}
     }
@@ -84,27 +84,30 @@ void matrixMultVinograd(int **first, int **second, int **result, int size) {
 	for(int i = 0; i < size; i++) //2 + size(2 + ...)
 		for(int j = 0; j < size; j++) //2 + size(2 + ...)
 		{
-			result[i][j] = -multH[i]- multV[j]; // 2+1+1+1+1+1=7 
+			result[i][j] = -multH[i]- multV[j]; // 2+1+1+1+1+1=7
 			for(int k = 0; k < size/2; k++){ //3 + size/2*(3 + ...)
 				result[i][j] = result[i][j] + \
 (first[i][2*k] + second[2*k+1][j])*(first[i][2*k+1] + second[2*k][j]);
 			}//2+1+2+1+3+1+4+1+4+1+3 = 23
 		}
-	
+
 	if(size%2 == 1) //2
 		for(int i =0; i< size; i++) // 2 + size*(2 + ...)
 			for(int j = 0; j< size; j++) // 2 + size*(2 + ...)
 				result[i][j] = result[i][j] + first[i][size-1]*second[size-1][j]; //2+1+2+1+3+1+3 = 13
-} 
+
+	free(multH);
+	free(multV);
+}
 
 
 void matrixMultVinogradUpgr(int **first, int **second, int **result, int size){
-  
-	int *multH = (int*)malloc(size*sizeof(int));	
+
+	int *multH = (int*)malloc(size*sizeof(int));
 	int* multV = (int*)malloc(size*sizeof(int));
-	
+
 	int hsize = size/2;
-	
+
 	for(int i = 0; i < size; i++){
 		multH[i] = 0;
 		for(int j = 0; j < hsize; j++)
@@ -116,19 +119,22 @@ void matrixMultVinogradUpgr(int **first, int **second, int **result, int size){
 		for(int j = 0; j < hsize; j++)
 			multV[i] += second[2*j][i]*second[2*j+1][i];
 	}
-	
+
 	int buf;
 	for(int i = 0; i < size; i++)
 		for(int j = 0; j < size; j++){
 			buf = 0 -multH[i]- multV[j];
 			for(int k = 0; k < hsize; k++)
-				buf += (first[i][2*k] + second[2*k+1][j])*(first[i][2*k+1] + second[2*k][j]);	
-			
+				buf += (first[i][2*k] + second[2*k+1][j])*(first[i][2*k+1] + second[2*k][j]);
+
 			result[i][j] = buf;
 		}
-	
+
 	if(size%2 == 1)
 		for(int i =0; i< size; i++)
 			for(int j = 0; j< size; j++)
 				result[i][j] += first[i][size-1]*second[size-1][j];
+
+				free(multH);
+				free(multV);
 }
