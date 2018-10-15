5d1a89ad93a798b8d5f8f0016bf7578d23b9cf93
chudilo <chudik97@yandex.ru>
2018-10-15 03:18:35 +0300

    done all tests and outputs

--- AA/lab2/main.cpp
+++ AA/lab2/main.cpp
@@ -3,51 +3,59 @@
 #include "matrix_func.h"
 #include "get_time.h"
 
-#define SIZE 1000
+//#define SIZE 1000
+#define TIMES 10
 
 int main() {
 
 	for(int i = 0; i < 20; i++) {
+		int SIZE = 100 + i*100;
 
 		int **matrA = matrixCreate(SIZE,SIZE);
-		matrixFillRand(matrA, SIZE, SIZE);
-		//matrixPrint(matrA, SIZE, SIZE);
-
 		int **matrB = matrixCreate(SIZE,SIZE);
-		matrixFillRand(matrB, SIZE, SIZE);
-		//matrixPrint(matrB, SIZE, SIZE);
-
-		double t1, t2, restime;
-
 		int **resMatr = matrixCreate(SIZE,SIZE);
 
-		matrixFillZero(resMatr, SIZE, SIZE);
-		restime = 0;
-		t1 =  getCPUTime( );
-		matrixMult(matrA,matrB,resMatr,SIZE);
-		t2 =  getCPUTime( );
-		//i == 0 ? restime = (t2-t1) : restime = (restime + t2-t1)/2;
-		//matrixPrint(resMatr,SIZE,SIZE);
-		printf("%f\n", restime);
+		double t1, t2, restimeMult = 0, restimeVin = 0, restimeVinUpgr = 0;
 
-		matrixFillZero(resMatr, SIZE, SIZE);
-		restime = 0;
-		t1 =  getCPUTime( );
-		matrixMultVinograd(matrA,matrB,resMatr,SIZE);
-		t2 =  getCPUTime( );
-		restime = (t2-t1);
-		printf("%f\n", restime);
-		//matrixPrint(resMatr,SIZE,SIZE);
+		for(int j = 0; j < TIMES; j++) {
+			matrixFillRand(matrA, SIZE, SIZE);
+			matrixFillRand(matrB, SIZE, SIZE);
 
-		matrixFillZero(resMatr, SIZE, SIZE);
-		restime = 0;
-		t1 =  getCPUTime( );
-		matrixMultVinogradUpgr(matrA,matrB,resMatr,SIZE);
-		t2 =  getCPUTime( );
-		restime = (t2-t1);
-		printf("%f\n", restime);
-		//matrixPrint(resMatr,SIZE,SIZE);
+			matrixFillZero(resMatr, SIZE, SIZE);
 
+			t1 =  getCPUTime( );
+			matrixMult(matrA,matrB,resMatr,SIZE);
+			t2 =  getCPUTime( );
+			//matrixPrint(resMatr,SIZE,SIZE);
+			restimeMult += (t2-t1);
+
+
+			matrixFillZero(resMatr, SIZE, SIZE);
+
+			t1 =  getCPUTime( );
+			matrixMultVinograd(matrA,matrB,resMatr,SIZE);
+			t2 =  getCPUTime( );
+			//matrixPrint(resMatr,SIZE,SIZE);
+			restimeVin += (t2-t1);
+
+
+
+			matrixFillZero(resMatr, SIZE, SIZE);
+
+			t1 =  getCPUTime( );
+			matrixMultVinogradUpgr(matrA,matrB,resMatr,SIZE);
+			t2 =  getCPUTime( );
+			//matrixPrint(resMatr,SIZE,SIZE);
+			restimeVinUpgr += (t2-t1);
+
+
+		}
+
+		printf("Proc time for %d elements casual mult :\n%f\n", SIZE, restimeMult/TIMES);
+		printf("Proc time for %d elements Vinograd mult :\n%f\n", SIZE, restimeVin/TIMES);
+		printf("Proc time for %d elements upgrade Vinograd mult:\n%f\n",SIZE, restimeVinUpgr/TIMES);
+		puts("");
+		
 		matrixFree(matrA,SIZE);
 		matrixFree(matrB,SIZE);
 		matrixFree(resMatr,SIZE);
